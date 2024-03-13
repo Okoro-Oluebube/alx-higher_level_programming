42442619323f3abfb231e37cc5ae99503157ade3
Okoro-Oluebube <okorooluebube0000@yahoo.com>
2024-03-12 19:30:07 -0700

    correction on task 1(include the negative sign for negative numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,16 +3,14 @@ import random
 number = random.randint(-10000, 10000)
 num = str(number)
 string = "Last digit of {} is {} "
-for number in range(0, 10001):
-    if num[-1:] > '5':
-        print(string.format(number, num[-1:]) + "and is greater than 5")
-    elif num[-1:] == '0':
-        print(string.format(number, num[-1:]) + "and is 0")
-    elif num[-1:] < '6' and num[-1:] != '0':
-        print(string.format(number, num[-1:]) + "and is less than 6 and not 0")
+if num[-1:] > '5' and number > 0:
+    print(string.format(number, num[-1:]) + "and is greater than 5")
+elif num[-1:] == '0' and number == 0:
+    print(string.format(number, num[-1:]) + "and is 0")
+elif num[-1:] < '6' and num[-1:] != '0' and number > 0:
+    print(string.format(number, num[-1:]) + "and is less than 6 and not 0")
 string = "Last digit of {} is -{} "
-for number in range(-10001, 0):
-    if num[-1:] > '5':                                             print(string.format(number, num[-1:]) + "and is greater than 5")
-    elif num[-1:] == '0':
-        print(string.format(number, num[-1:]) + "and is 0")
-    elif num[-1:] < '6' and num[-1:] != '0':                       print(string.format(number, num[-1:]) + "and is less than 6 and not 0")
+if num[-1:] > '5' and number < 0:
+    print(string.format(number, num[-1:]) + "and is greater than 5")
+elif num[-1:] < '6' and num[-1:] != '0' and number < 0:
+    print(string.format(number, num[-1:]) + "and is less than 6 and not 0")
