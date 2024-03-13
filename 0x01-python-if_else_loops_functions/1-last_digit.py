15b9e97669245ae59654b15685e8f5556094d46c
Okoro-Oluebube <okorooluebube0000@yahoo.com>
2024-03-12 21:53:04 -0700

    mod on task 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -11,6 +11,6 @@ elif num[-1:] < '6' and num[-1:] != '0' and number > 0:
     print(string.format(number, num[-1:]) + "and is less than 6 and not 0")
 string = "Last digit of {} is -{} "
 if num[-1:] > '5' and number < 0:
-    print(string.format(number, num[-1:]) + "and is greater than 5")
+    print(string.format(number, num[-1:]) + "and is less than 6 and not 0")
 elif num[-1:] < '6' and num[-1:] != '0' and number < 0:
     print(string.format(number, num[-1:]) + "and is less than 6 and not 0")
