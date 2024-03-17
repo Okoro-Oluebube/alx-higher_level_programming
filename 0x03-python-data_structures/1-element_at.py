f5efe678c4b8d614e33679fe952565b25b2066b6
Okoro-Oluebube <okorooluebube0000@yahoo.com>
2024-03-17 06:50:57 -0700

    mod on task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,7 +2,7 @@
 
 def element_at(my_list, idx):
     i = len(my_list)
-    if idx < 0 or idx > i:
+    if idx < 0 or idx >= i:
         return (None)
     else:
-        return (my_list[i])
+        return (my_list[idx])
