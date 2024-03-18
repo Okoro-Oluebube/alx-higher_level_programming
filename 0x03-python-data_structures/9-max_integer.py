3766209ce8e581f69356eb020c80e4d00050223c
Okoro-Oluebube <okorooluebube0000@yahoo.com>
2024-03-18 14:43:36 -0700

    mod om task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
-    for j in my_list:
+    if my_list ==[]:
+        return (None)
+    else:
+        j = my_list[0]
         for i in range(1, len(my_list)):
-            if j > my_list[i]:
-                list1 = j
-            else:
-                list1 = my_list[i]
-            break
-    return (list1)
+            if my_list[i] > j:
+                j = my_list[i]
+        return (j)
