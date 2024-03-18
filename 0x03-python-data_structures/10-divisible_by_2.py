11f35748c5748379c716453a0eb6d682a1616260
Okoro-Oluebube <okorooluebube0000@yahoo.com>
2024-03-18 14:08:19 -0700

    task 12

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def divisible_by_2(my_list=[]):
+    new_list = my_list[:]
+    for i in my_list:
+        if i % 2 == 0:
+            print(new_list)
+            return (new_list, True)
+            print(new_list)
+        else:
+            print(new_list)
+            return (new_list, False)
+            print(new_list)
+        print(new_list)
